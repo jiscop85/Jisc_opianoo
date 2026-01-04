8d672106c5e508f9ae93eae5cb22be83536166da
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2026-01-05 00:35:01 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+نقطه ورود برنامه Piano Master Tutor
+"""
+import sys
+from PyQt6.QtWidgets import QApplication
+from PyQt6.QtCore import Qt
+
+from src.gui.main_window import MainWindow
+from src.utils.logger import logger
+
+
+
