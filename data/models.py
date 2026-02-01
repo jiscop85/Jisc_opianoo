b81c69edcd74708202ee1dafbe08e28803bf2733
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2026-02-02 00:24:39 +0330

    Add files via upload

---
+++ data/models.py
@@ -0,0 +1,10 @@
+"""
+مدل‌های دیتابیس SQLAlchemy
+"""
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey, Text, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from datetime import datetime
+
+Base = declarative_base()
+
